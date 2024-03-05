cff150bd44cc739eebf9fbafac4671c2f6046442
Eduardo Andrade <closekeycompany@gmail.com>
2024-03-04 21:00:34 -0300

    Traduzir blade de email de código de verificação

--- resources/views/mail/code-verify.blade.php
+++ resources/views/mail/code-verify.blade.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Verify Your Email Address</title>
+    <title>Verifique seu Endereço de Email</title>
 </head>
 <body>
-    <h1>Verify Your Email Address</h1>
-    <p>Thank you for registering! Please use the following code to verify your email address:</p>
+    <h1>Verifique seu Endereço de Email</h1>
+    <p>Obrigado por se registrar! </p>
+    <p>Por favor, utilize o seguinte código para verificar seu endereço de email:</p>
     <p><strong>{{ $user->code_verify }}</strong></p>
-    <p>If you did not register for an account, please ignore this email.</p>
+    <p>Se você não se registrou para uma conta, por favor, ignore este email.</p>
 </body>
 </html>
